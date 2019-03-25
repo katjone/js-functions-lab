0b162a12d972804026999f1b4e2fe215aec794fa
Nice Lady <kjone203.e@gmail.com>
2019-03-25 16:55:31 -0700

    Adds question 1 solution

--- starter_code/main.js
+++ starter_code/main.js
@@ -1,6 +1,10 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
 }
 
 // Question 2
